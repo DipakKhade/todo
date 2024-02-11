39424add9e975a6555cbadb6b18d4d4478d573e5
= <=>
2024-02-11 19:29:34 +0530

    added prisma client

---
+++ lib/todo.ts
@@ -0,0 +1,11 @@
+import prisma from "./prisma";
+
+export async function getTodos(){
+    try {
+        const todos = prisma.todo.findMany()
+        return {todos}
+    } catch (error) {
+        return {error}
+        
+    }
+}
